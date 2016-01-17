7828695f3f8ac526808f93291963bc26195c4e01
sstern6 <scott.stern06@gmail.com>
2016-01-17 15:12:15 -0800

    Implement ng-click feature

--- app/public/app.js
+++ app/public/app.js
@@ -1,15 +1,41 @@
 var blog = angular.module('blog',['ngRoute'])
 
-    function BlogController($scope,commentService){
+    function BlogController($scope,$location,commentService){
       $scope.blogs =  [
         {id:1,
+        subject:'JavaScript',
         title:'First Blog Post on My New Angular Blog Site',
         post:'Welcome to my blog!!! I have been focusing a lot on JavaScript lately so the following posts might be heavy in JS. Today I am going to talk about .sort()...',
-        date:'12/1/15'}
+        date:'12/1/15'},
+        {id:2,
+        subject:'JavaScript',
+        title:'Javascript',
+        post:'blah blah blah',
+        date:'1/17/16'},
+        {id:3,
+        title:'Ruby',
+        subject:'Ruby',
+        post:'blah blah blah',
+        date:'1/17/16'},
+        {id:4,
+        title:'SQL Server',
+        subject:'SQL',
+        post:'blah blah blah',
+        date:'1/17/16'}
+
       ],
       $scope.addNewComment = function(){
           commentService.addComment($scope.comment)
       }
+
+      $scope.focus = function($event){
+        var id = $event.target.id;
+      }
+
+      $scope.go = function(path){
+        $location.path(path)
+      }
+
     };
 
 
@@ -37,18 +63,30 @@ var blog = angular.module('blog',['ngRoute'])
           url:'/',
           data:{"comment":comment}
         })
-
-        .then(function(){
-
-        })
-
       }
 
       return({
         addComment:addComment
       });
-
     })
+
+    // angular.module('homeDirective',[])
+    // .controller('homeDirectiveController',['$scope','$location',function($scope){
+    //   $scope.phrases = {
+    //     one:"hello"
+    //   }
+
+    // }])
+    // .directive('goHome',function(){
+    //   return{
+    //     template:"{{phrases.one}}"
+    //   }
+    // })
+
+
+
+
+
 
 
 
